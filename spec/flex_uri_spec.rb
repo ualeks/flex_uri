846f877f67437db99c6b93e38aef5ebdbbfc5391
Aleks Ulanov <sashachilly.z@gmail.com>
2023-04-30 08:48:37 -0400

    initial commit

---
+++ spec/flex_uri_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe FlexUri do
+  it "has a version number" do
+    expect(FlexUri::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
